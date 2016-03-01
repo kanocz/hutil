8a374d4ba552cb1a13f23085f2501d199d893c26
Anton Skorokhod <anton@nsl.cz>
2016-03-01 22:07:27 +0100

    slack.go now uses webhook proto instead of slackbot

--- slack.go
+++ slack.go
@@ -2,34 +2,67 @@ package hutil
 
 import (
 	"bytes"
+	"encoding/json"
+	"errors"
 	"log"
 	"net/http"
+	"strings"
 )
 
 var (
-	slackurl string
+	slackurl   string
+	slackname  string
+	slackimg   string
+	slackemoji string
 )
 
 // SlackInit function sets slack team, token and channel for logging
-func SlackToken(team, token, channel string) {
-	slackurl = "https://" + team + ".slack.com/services/hooks/slackbot?token=" + token + "&channel=%23" + channel
+func SlackInit(url, name, icon string) {
+	slackurl = url
+	slackname = name
+	if strings.HasPrefix(icon, ":") {
+		slackemoji = icon
+	} else {
+		slackurl = icon
+	}
 }
 
 // SlackLog sends log message to slack's #log channel
-func SlackLog(message string) {
+func SlackLog(message string) error {
+	if "" == slackurl {
+		return errors.New("Slack url is not set")
+	}
 	go func() {
-		if "" == slackurl {
-			log.Println("Slack token is not set yet")
+		data := map[string]string{"text": message}
+		if "" != slackname {
+			data["username"] = slackname
+		}
+		if "" != slackemoji {
+			data["icon_emoji"] = slackemoji
+		}
+		if "" != slackurl {
+			data["icon_url"] = slackurl
+		}
+
+		jsondata, err := json.Marshal(data)
+
+		if nil != err {
+			log.Println("Error encoding json for slack message:", err)
 			return
 		}
-		req, err := http.NewRequest("POST", slackurl, bytes.NewBuffer([]byte(message)))
-		if nil == err {
-			client := &http.Client{}
-			resp, err := client.Do(req)
-			if err != nil {
-				log.Println("Error sending message to slack:", err)
-			}
-			resp.Body.Close()
+
+		req, err := http.NewRequest("POST", slackurl, bytes.NewBuffer(jsondata))
+		if nil != err {
+			log.Println("Error while http.NewRequest for slack message:", err)
+			return
 		}
+
+		client := &http.Client{}
+		resp, err := client.Do(req)
+		if err != nil {
+			log.Println("Error sending message to slack:", err)
+		}
+		resp.Body.Close()
 	}()
+	return nil
 }
