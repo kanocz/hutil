541bbb2603fd2f5e2e99bd972bfaf61558ae86c1
Anton Skorokhod <anton@nsl.cz>
2016-03-02 23:31:10 +0100

    Add SessionInfo struct generated msgp

---
+++ session_gen.go
@@ -0,0 +1,56 @@
+package hutil
+
+// NOTE: THIS FILE WAS PRODUCED BY THE
+// MSGP CODE GENERATION TOOL (github.com/tinylib/msgp)
+// DO NOT EDIT
+
+import (
+	"github.com/tinylib/msgp/msgp"
+)
+
+// MarshalMsg implements msgp.Marshaler
+func (z SessionInfo) MarshalMsg(b []byte) (o []byte, err error) {
+	o = msgp.Require(b, z.Msgsize())
+	// map header, size 1
+	// string "uid"
+	o = append(o, 0x81, 0xa3, 0x75, 0x69, 0x64)
+	o = msgp.AppendInt64(o, z.UID)
+	return
+}
+
+// UnmarshalMsg implements msgp.Unmarshaler
+func (z *SessionInfo) UnmarshalMsg(bts []byte) (o []byte, err error) {
+	var field []byte
+	_ = field
+	var isz uint32
+	isz, bts, err = msgp.ReadMapHeaderBytes(bts)
+	if err != nil {
+		return
+	}
+	for isz > 0 {
+		isz--
+		field, bts, err = msgp.ReadMapKeyZC(bts)
+		if err != nil {
+			return
+		}
+		switch msgp.UnsafeString(field) {
+		case "uid":
+			z.UID, bts, err = msgp.ReadInt64Bytes(bts)
+			if err != nil {
+				return
+			}
+		default:
+			bts, err = msgp.Skip(bts)
+			if err != nil {
+				return
+			}
+		}
+	}
+	o = bts
+	return
+}
+
+func (z SessionInfo) Msgsize() (s int) {
+	s = 1 + 4 + msgp.Int64Size
+	return
+}
